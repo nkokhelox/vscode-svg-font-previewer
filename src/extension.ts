2e7f4b0873e8f93a61f42e4b170600e69734fe63
Nkokhelo Mhlongo <nkokhelox@gmail.com>
2021-12-19 14:00:18 +0200

    only work for the SVG Fonts, not for all SVGs

--- src/extension.ts
+++ src/extension.ts
@@ -57,8 +57,8 @@ export function activate(context: vscode.ExtensionContext) {
             'extension.svgFontPreview',
             () => {
                 const editorView = vscode.window.activeTextEditor;
-                if (editorView && isSvg(editorView.document)) {
-                    activatePreviewPanel(context, editorView.document, true);
+                if (editorView) {
+                    activatePreviewPanel(context, editorView.document, false, true);
                 }
             }
         )
@@ -85,27 +85,41 @@ export function activate(context: vscode.ExtensionContext) {
 
     vscode.workspace.onDidOpenTextDocument(
         (document: vscode.TextDocument) => {
-            if (autoOpenPreview && isSvg(document)) {
-                activatePreviewPanel(context, document);
+            if (autoOpenPreview) {
+                activatePreviewPanel(context, document, true);
             }
         }
     );
 }
 
-function activatePreviewPanel(context: vscode.ExtensionContext, document: vscode.TextDocument, refreshContent: boolean = false) {
-    const fileName = getFileName(document);
-    const panel = getWebViewPanel(fileName, context);
-    if (panel) {
-        if (refreshContent || panel.webview.html === undefined || panel.webview.html === null) {
-            const htmlContentString = previewSvg(document);
-            if (htmlContentString) {
-                panel.webview.html = htmlContentString;
-            } else {
-                vscode.window.showInformationMessage(`'${fileName}' is not the SVG file`);
+function activatePreviewPanel(context: vscode.ExtensionContext, document: vscode.TextDocument, isAutoActivation: boolean, refreshContent: boolean = false) {
+    if (isSvg(document)) {
+        const fileName = getFileName(document);
+        const editorSvgContent = document.getText();
+
+        const parser = new xml.DOMParser();
+        const xmlFontContent = parser.parseFromString(editorSvgContent, `text/xml`);
+        const fontNodes = xmlFontContent.getElementsByTagName('font');
+
+        if (!fontNodes || fontNodes.length <= 0) { // Normal svg image
+            !isAutoActivation && showInvalidFontFile(getFileName(document));
+        } else { // Font svg
+            const panel = getWebViewPanel(fileName, context);
+            if (panel) {
+                if (refreshContent || panel.webview.html === undefined || panel.webview.html === null) {
+                    const htmlContentString = previewSvgFont(parser, xmlFontContent)
+                    if (htmlContentString) {
+                        panel.webview.html = htmlContentString;
+                    } else {
+                        vscode.window.showInformationMessage(`'${fileName}' is not the SVG file`);
+                    }
+                }
+
+                panel.reveal(panel.viewColumn);
             }
         }
-
-        panel.reveal(panel.viewColumn);
+    } else {
+        !isAutoActivation && showInvalidFontFile(getFileName(document));
     }
 }
 
@@ -133,43 +147,31 @@ function getWebViewPanel(
     }
 }
 
-function previewSvg(document: vscode.TextDocument): string | undefined {
-    let htmlContentString = undefined;
+function previewSvgFont(parser: xml.DOMParser, xmlFontContent: Document): string | undefined {
+    // Setup the html to show in the preview
+    const htmlDocument = parser.parseFromString('<!doctype html>', `text/html`);
+    const htmlBody = htmlDocument.createElement(`body`);
 
-    if (isSvg(document)) {
-        const editorSvgContent = document.getText();
+    // Parsing the font icons and building the preview html
+    const fontNodes = xmlFontContent.getElementsByTagName('font');
+    for (let fontIndex = 0; fontNodes && fontIndex < fontNodes.length; fontIndex++) {
+        const fontNode = fontNodes[fontIndex];
+        const fontFamily = fontNode.getAttribute('id');
+        const fontHorizAdvX = fontNode.getAttribute('horiz-adv-x');
 
-        const parser = new xml.DOMParser();
-        const xmlFontContent = parser.parseFromString(editorSvgContent, `text/xml`);
-        const htmlDocument = parser.parseFromString('<!doctype html>', `text/html`);
+        const fontFace = fontNode.getElementsByTagName('font-face')[0];
+        const unitsPerEm = fontFace.getAttribute('units-per-em') || "1";
+        const descent = fontFace.getAttribute('descent');
+        const ascent = fontFace.getAttribute('ascent');
 
-        const fontNodes = xmlFontContent.getElementsByTagName('font');
+        try {
+            const fontMetaData = parser.parseFromString(`<p><i>${xmlFontContent.getElementsByTagName('metadata')[0].textContent}</i></p>`);
+            htmlBody.appendChild(fontMetaData);
+        } catch (error) {
+            console.error(error);
+        }
 
-        const htmlBody = htmlDocument.createElement(`body`);
-
-        let renderContent = false;
-
-        if (!fontNodes || fontNodes.length <= 0) { // Normal svg image
-            vscode.window.showInformationMessage(`'${getFileName(document)}' is not the Font SVG file`);
-        } else { // Font svg
-            for (let fontIndex = 0; fontNodes && fontIndex < fontNodes.length; fontIndex++) {
-                const fontNode = fontNodes[fontIndex];
-                const fontFamily = fontNode.getAttribute('id');
-                const fontHorizAdvX = fontNode.getAttribute('horiz-adv-x');
-
-                const fontFace = fontNode.getElementsByTagName('font-face')[0];
-                const unitsPerEm = fontFace.getAttribute('units-per-em');
-                const descent = fontFace.getAttribute('descent');
-                const ascent = fontFace.getAttribute('ascent');
-
-                try {
-                    const fontMetaData = parser.parseFromString(`<p><i>${xmlFontContent.getElementsByTagName('metadata')[0].textContent}</i></p>`);
-                    htmlBody.appendChild(fontMetaData);
-                } catch (error) {
-                    console.error(error);
-                }
-
-                const fontDescriptionElement = parser.parseFromString(`
+        const fontDescriptionElement = parser.parseFromString(`
                     <ul>
                         <li><b>font name:</b> ${fontFamily}</li>
                         <li><b>horiz-adv-x:</b> ${fontHorizAdvX}</li>
@@ -178,83 +180,86 @@ function previewSvg(document: vscode.TextDocument): string | undefined {
                         <li><b>descent:</b> ${descent}</li>
                     </ul>
                 `);
-                htmlBody.appendChild(fontDescriptionElement);
+        htmlBody.appendChild(fontDescriptionElement);
 
-                const glyphList = fontNode.getElementsByTagName('glyph');
-                let fontIcons = [];
+        const glyphList = fontNode.getElementsByTagName('glyph');
+        let fontIcons = [];
 
-                for (let fontIconIndex = 0; fontIconIndex < glyphList.length; fontIconIndex++) {
-                    renderContent = true;
+        for (let fontIconIndex = 0; fontIconIndex < glyphList.length; fontIconIndex++) {
 
-                    const glyphIcon = glyphList[fontIconIndex];
-                    if (glyphIcon) {
-                        const svgPathData = glyphIcon.getAttribute('d');
-                        const unicodeChar = glyphIcon.getAttribute('unicode');
-                        const iconName = glyphIcon.getAttribute('glyph-name');
-                        const horizontalUnits = glyphIcon.getAttribute('horiz-adv-x') || unitsPerEm;
-                        const hexChar = unicodeChar ? (unicodeChar.charCodeAt(0).toString(16)) : null;
+            const glyphIcon = glyphList[fontIconIndex];
+            if (glyphIcon) {
+                const svgPathData = glyphIcon.getAttribute('d');
+                const unicodeChar = glyphIcon.getAttribute('unicode');
+                const iconName = glyphIcon.getAttribute('glyph-name') || '????';
+                const horizontalUnits = glyphIcon.getAttribute('horiz-adv-x') || unitsPerEm;
+                const hexChar = unicodeChar ? (unicodeChar.charCodeAt(0).toString(16)) : "";
 
-                        if (svgPathData) {
-                            const pathElement = htmlDocument.createElement(`path`);
-                            pathElement.setAttribute('transform', `translate(0,${unitsPerEm}) scale(1, -1)`);
-                            pathElement.setAttribute('style', 'fill:currentcolor');
-                            pathElement.setAttribute('d', svgPathData);
+                const widthInt = parseInt(horizontalUnits);
 
-                            const svgElement = htmlDocument.createElement(`svg`);
-                            svgElement.setAttribute('viewBox', `0 0 ${(horizontalUnits * 1) * 1.2} ${(unitsPerEm * 1) * 1.2}`);
-                            svgElement.setAttribute('style', 'height:4em');
-                            svgElement.appendChild(pathElement);
+                let emWidth = (widthInt > 1024) ? (widthInt / 320) : 4;
 
-                            const iconContainer = htmlDocument.createElement('a');
-                            iconContainer.setAttribute('style', `text-decoration:none; color:inherit; display:block; margin: auto auto 30px auto; width:3em; height:4em; padding:.5em;`);
-                            iconContainer.setAttribute('href', '#${iconName}');
-                            iconContainer.setAttribute('name', iconName);
-                            iconContainer.appendChild(svgElement);
+                if (svgPathData) {
+                    const pathElement = htmlDocument.createElement(`path`);
+                    pathElement.setAttribute('transform', `translate(0,${unitsPerEm}) scale(1, -1)`);
+                    pathElement.setAttribute('style', 'fill:currentcolor');
+                    pathElement.setAttribute('d', svgPathData);
 
-                            const iconSvgPath = htmlDocument.createElement('dt');
-                            iconSvgPath.setAttribute('style', 'margin:0');
-                            iconSvgPath.setAttribute('class', 'glyph');
-                            iconSvgPath.appendChild(iconContainer);
+                    const svgElement = htmlDocument.createElement(`svg`);
+                    svgElement.setAttribute('viewBox', `0 0 ${((+horizontalUnits) * 1) * 1.2} ${((+unitsPerEm) * 1) * 1.2}`);
+                    svgElement.setAttribute('style', 'height:4em');
+                    svgElement.appendChild(pathElement);
 
-                            const iconSvgName = htmlDocument.createElement(`span`);
-                            iconSvgName.setAttribute('id', `icon-name-${iconName}`);
-                            iconSvgName.setAttribute('style', `margin: 0; bottom: 0; font-size:15px; overflow:hidden; width:100%;`);
-                            iconSvgName.appendChild(htmlDocument.createTextNode(iconName));
+                    const iconContainer = htmlDocument.createElement('a');
+                    iconContainer.setAttribute('style', `text-decoration:none; color:inherit; display:block; margin: auto auto 30px auto; width:${emWidth}em; height:4em; padding:.5em;`);
+                    iconContainer.setAttribute('href', '#${iconName}');
+                    iconContainer.setAttribute('name', iconName);
+                    iconContainer.appendChild(svgElement);
 
-                            const iconSvgChar = htmlDocument.createElement(`p`);
-                            iconSvgChar.setAttribute('id', `icon-char-${hexChar}`);
-                            iconSvgChar.setAttribute('style', `font-size:10px; overflow:hidden; width:100%;`);
-                            iconSvgChar.appendChild(htmlDocument.createTextNode(hexChar));
+                    const iconSvgPath = htmlDocument.createElement('dt');
+                    iconSvgPath.setAttribute('style', 'margin:0');
+                    iconSvgPath.setAttribute('class', 'glyph');
+                    iconSvgPath.appendChild(iconContainer);
 
-                            const svgContent = htmlDocument.createElement(`dl`);
-                            svgContent.setAttribute('style', 'margin: 0 0 .5em .5em; float: left; outline: currentcolor dotted 1px; min-width: 10em; min-height: 10em; padding: .5em;');
-                            svgContent.setAttribute('id', iconName);
-                            svgContent.appendChild(iconSvgChar);
-                            svgContent.appendChild(iconSvgPath);
-                            svgContent.appendChild(iconSvgName);
+                    const glyphDiv = htmlDocument.createElement(`div`);
+                    glyphDiv.appendChild(iconSvgPath);
+                    glyphDiv.setAttribute('style', `text-align: center;margin-left: auto;margin-right: auto;`);
 
-                            fontIcons.push(new SortableTag(iconName, hexChar, svgContent));
-                        }
-                    }
+                    const iconSvgName = htmlDocument.createElement(`span`);
+                    iconSvgName.setAttribute('id', `icon-name-${iconName}`);
+                    iconSvgName.setAttribute('style', `margin: 0; bottom: 0; font-size:15px; overflow:hidden; width:100%;`);
+                    iconSvgName.appendChild(htmlDocument.createTextNode(iconName));
+
+                    const iconSvgChar = htmlDocument.createElement(`p`);
+                    iconSvgChar.setAttribute('id', `icon-char-${hexChar}`);
+                    iconSvgChar.setAttribute('style', `font-size:10px; overflow:hidden; width:100%;`);
+                    iconSvgChar.appendChild(htmlDocument.createTextNode(hexChar));
+
+                    const svgContent = htmlDocument.createElement(`dl`);
+                    svgContent.setAttribute('style', 'margin: 0 0 .5em .5em; float: left; outline: currentcolor dotted 1px; min-width: 10em; min-height: 10em; padding: .5em;');
+                    svgContent.setAttribute('id', iconName);
+                    svgContent.appendChild(iconSvgChar);
+                    svgContent.appendChild(glyphDiv);
+                    svgContent.appendChild(iconSvgName);
+
+                    fontIcons.push(new SortableTag(iconName, hexChar, svgContent));
                 }
-
-                const sortOrderFactor = sortByOrder === TagSortOrder.DESC ? -1 : 1;
-                fontIcons = sortByField === TagSortBy.NONE ? fontIcons : fontIcons.sort((a, b) => a.get(sortByField) < b.get(sortByField) ? -1 * sortOrderFactor : 1 * sortOrderFactor);
-                fontIcons.forEach(x => htmlBody.appendChild(x.element));
             }
         }
 
-        if (renderContent) {
-            const htmlContent = htmlDocument.createElement(`html`);
-            htmlContent.appendChild(parser.parseFromString(`<head><meta charset="UTF-8"><title>SVG font preview</title></head>`));
-            htmlContent.setAttribute('lang', 'en');
-            htmlContent.appendChild(htmlBody);
-            htmlDocument.appendChild(htmlContent);
-
-            htmlContentString = new xml.XMLSerializer().serializeToString(htmlDocument);
-        }
+        const sortOrderFactor = sortByOrder === TagSortOrder.DESC ? -1 : 1;
+        fontIcons = sortByField === TagSortBy.NONE ? fontIcons : fontIcons.sort((a, b) => a.get(sortByField) < b.get(sortByField) ? -1 * sortOrderFactor : 1 * sortOrderFactor);
+        fontIcons.forEach(x => htmlBody.appendChild(x.element));
     }
-    return htmlContentString;
+
+    const htmlContent = htmlDocument.createElement(`html`);
+    htmlContent.appendChild(parser.parseFromString(`<head><meta charset="UTF-8"><title>SVG font preview</title></head>`));
+    htmlContent.setAttribute('lang', 'en');
+    htmlContent.appendChild(htmlBody);
+    htmlDocument.appendChild(htmlContent);
+
+    return new xml.XMLSerializer().serializeToString(htmlDocument);
+
 }
 
 function loadConfig() {
@@ -272,4 +277,8 @@ function getFileName(document: vscode.TextDocument): string {
 function isSvg(document: vscode.TextDocument): boolean {
     const lowerLanguageId = document.languageId.trim().toLowerCase();
     return lowerLanguageId == 'svg' || lowerLanguageId === 'xml' && getFileName(document).trim().toLowerCase().endsWith('.svg');
+}
+
+function showInvalidFontFile(documentName: string): void {
+    vscode.window.showInformationMessage(`'${documentName}' is not a valid SVG Font file.`);
 }
